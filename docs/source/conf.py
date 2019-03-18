d129ca9faf2d3ffd494dff585f0e75a60ded7503
vCra <aaw13@aber.ac.uk>
2019-03-18 18:59:04 +0000

    Try and get a nice pretty theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,10 +74,6 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +101,6 @@ html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'mooshak2apidoc'
-
 
 # -- Options for LaTeX output ------------------------------------------------
 
